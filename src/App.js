d4d84b5784aa70357ebaad48885f1aacec0c5ed6
groomigroom <googjohn0143@gmail.com>
2026-03-11 21:51:28 +0000

    header_module

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './img/insang_biseo_logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <object data={logo} type='image/svg+xml' className='app_logo'></object>
-      </header>
+      
     </div>
   );
 }
--- src/component/header.js
+++ src/component/header.js
@@ -1,7 +1,11 @@
-export default function header() {
+import logo from '../img/insang_biseo_logo.svg';
+
+export default function Header() {
     return (
         <>
-        
+            <header className="App-header">
+                <object data={logo} type='image/svg+xml' className='app_logo'></object>
+            </header>
         </>
     );
 }
