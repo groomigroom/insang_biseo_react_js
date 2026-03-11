9c010c3fa178c39631d5703357782c680f1d7e2f
groomigroom <googjohn0143@gmail.com>
2026-03-11 22:22:16 +0000

    aria-label: 로고

--- src/component/header.js
+++ src/component/header.js
@@ -4,8 +4,8 @@ import logo from '../img/insang_biseo_logo.svg';
 export default function Header() {
     return (
         <>
-            <header className="App-header">
-                <object data={logo} type='image/svg+xml' className='app_logo'></object>
+            <header className="app_header">
+                <object data={logo} type='image/svg+xml' aria-label='로고' className='app_logo'></object>
             </header>
         </>
     );
