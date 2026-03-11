6a22c2c65330ba7d77d9ce5ba179b4adc5097964
groomigroom <googjohn0143@gmail.com>
2026-03-11 11:59:39 +0000

    object_tag_end

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <object data={logo} type='image/svg+xml'></object>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
