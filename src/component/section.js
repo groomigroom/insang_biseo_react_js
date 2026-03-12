e95c3349423ca5ccfa6bd4cfc1fd50dac2a9f543
groomigroom <googjohn0143@gmail.com>
2026-03-12 07:16:28 +0000

    pp_connect

--- src/component/section.js
+++ src/component/section.js
@@ -1,8 +1,10 @@
+import pp from './section_pp';
+
 export default function Section() {
     return (
         <>
             <section className="app_section">
-                
+                <pp />
             </section>
         </>
     );
