b323b8446e8e2c02659dd215c558e38037ebceea
groomigroom <googjohn0143@gmail.com>
2026-03-12 09:09:00 +0000

    footer_part plus

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './component/header';
 import Section_part from './component/section';
+import Footer_part from './component/footer_part';
 
 function App() {
   return (
     <div className="App">
-      <Header className="header_part" />
+      <Header />
       <Section_part />
+      <Footer_part />
     </div>
   );
 }
---
+++ src/component/footer_part.js
@@ -0,0 +1,7 @@
+export default function Footer_part() {
+    return (
+        <>
+            <button>자세히 보기</button>
+        </>
+    );
+}
