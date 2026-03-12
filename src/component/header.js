38cb81d9b424bb59ac142ee00b2d6a303f578290
groomigroom <googjohn0143@gmail.com>
2026-03-12 23:41:36 +0000

    console.log

--- src/component/header.js
+++ src/component/header.js
@@ -2,13 +2,17 @@ import './header.css';
 import logo from '../img/insang_biseo_logo.svg';
 
 export default function Header() {
+    function showName() {
+        console.log("groom");
+    }
+
     return (
         <>
             <header className="app_header">
                 <div>
                     <img src={logo} alt='로고' className='app_logo' />
                 </div>
-                <div></div>
+                <div onClick={showName}></div>
             </header>
         </>
     );
